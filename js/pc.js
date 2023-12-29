282f0f7c9cd3d7ee970a11b4551e4114280d8ada
hoon0801 <141212683+hoon0801@users.noreply.github.com>
2023-12-29 12:59:01 +0900

    21th

--- js/pc.js
+++ js/pc.js
@@ -53,24 +53,24 @@ function pc(){
     });
       
       // s4
-      const highlight = document.getElementById("highlight-style");
+      // const highlight = document.getElementById("highlight-style");
   
-      gsap.registerPlugin(ScrollTrigger);
+      // gsap.registerPlugin(ScrollTrigger);
       
-      gsap.utils.toArray(".text-highlight").forEach((highlight) => {
-        ScrollTrigger.create({
-          trigger: highlight,
-          start: "-100px center",
-          onEnter: () => highlight.classList.add("active")
-        });
-      });
+      // gsap.utils.toArray(".text-highlight").forEach((highlight) => {
+      //   ScrollTrigger.create({
+      //     trigger: highlight,
+      //     start: "-100px center",
+      //     onEnter: () => highlight.classList.add("active")
+      //   });
+      // });
       
-      const setHighlightStyle = (value) =>
-        document.body.setAttribute("data-highlight", value);
+      // const setHighlightStyle = (value) =>
+      //   document.body.setAttribute("data-highlight", value);
       
-      highlight.addEventListener("change", (e) => setHighlightStyle(e.target.value));
+      // highlight.addEventListener("change", (e) => setHighlightStyle(e.target.value));
       
-      setHighlightStyle(highlight.value);
+      // setHighlightStyle(highlight.value);
       // s4
   
       document.addEventListener("mousemove", function(event) {
