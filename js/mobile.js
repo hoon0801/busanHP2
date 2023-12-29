c7349e0c428a9386e53f24712645a1442923d460
hoon0801 <141212683+hoon0801@users.noreply.github.com>
2023-12-29 11:30:52 +0900

    17th

--- js/mobile.js
+++ js/mobile.js
@@ -13,13 +13,13 @@ function mobile(){
             $('nav div').css({
                 'transform' : 'translateX(0)'
             })
-           },200);
+           });
         }else{
             setTimeout(function(){
                 $('nav div').css({
                     'transform' : 'translateX(-100%)'
                 })
-               },500);
+               });
         }
         $('nav').stop().slideToggle();
         $(this).toggleClass('close');
